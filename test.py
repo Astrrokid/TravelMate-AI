3c9a55dbcdb388312e178a3ed94f55591b63a3a2
Astrrokid <kidstarro22@gmail.com>
2026-09-12 16:37:35 +0100

    added app

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_graph
 
 # res = tavily_search("Best travel destinations in Europe")
 # print(res)
 
-res = search_flights("plan a 7 days japan trip from Lagos, Nigeria")
-print(res)
+# res = search_flights("plan a 7 days japan trip from Lagos, Nigeria")
+# print(res)
+user_input = input("Enter your travel request: ")
+response = run_travel_graph(user_input=user_input, thread_id="test_user")
+
+print(response["answer"])
